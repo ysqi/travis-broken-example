d724b91a0a9de651399466f7d1a03bc5785b59b3
ysqi <devysq@gmail.com>
2016-11-03 22:05:35 +0800

    fixed GH_REF

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,21 +11,8 @@ fi
  
 REPO=$(git config remote.origin.url)
  
-if [ -n "$TRAVIS_BUILD_ID" ]; then
-  # When running on Travis we need to use SSH to deploy to GitHub
-  #
-  # The following converts the repo URL to an SSH location,
-  # decrypts the SSH key and sets up the Git config with
-  # the correct user name and email (globally as this is a
-  # temporary travis environment)
-  #
-  # Set the following environment variables in the travis configuration (.travis.yml)
-  #
-  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-  #   GIT_NAME         - The Git user name
-  #   GIT_EMAIL        - The Git user email
-  #
+if [ -n "$TRAVIS_BUILD_ID" ]; then 
+
   echo DEPLOY_BRANCH: $DEPLOY_BRANCH
   echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
   echo GIT_NAME: $GIT_NAME
